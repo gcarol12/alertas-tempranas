0e5433715c7d8142801fd9f133cbf3d40d9d4925
carolina galvis arango <btcarol.gal@gmail.com>
2021-12-03 00:15:53 -0500

    agrego archivos faltantes

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //$this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Carolina Galvis Arango',
+            'email' => 'cga@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Post::factory()->count(24)->create();
+    }
+
+}
