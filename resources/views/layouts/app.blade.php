e140c962a8af54d82554ce3ea08e0fe5b4e0a1ad
carolina galvis arango <btcarol.gal@gmail.com>
2021-12-03 23:06:47 -0500

    comienza edicion footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'AT') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -81,7 +81,12 @@
         </nav>
 
         <main class="py-4">
+
+            @include('layouts.header')
+            
             @yield('content')
+           
+            @include('layouts.footer')
         </main>
     </div>
 </body>
